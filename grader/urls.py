2f6db2977a55e045ddf35a8e80418edbe86fd969
dominik martinez <dominikmartinez@pm.me>
2025-01-31 14:32:32 -0800

    Update for django 5

--- grader/urls.py
+++ grader/urls.py
@@ -1,42 +1,29 @@
 """acdec URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.9/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.urls import re_path
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name="index"),
-    url(r'^login/$', views.login_view, name="login"),
-    url(r'^logout/$', views.logout_view, name="logout"),
-    url(r'^speech/$', views.speech, name="speech"),
-    url(r'^interview/$', views.interview, name="interview"),
-    url(r'^export/$', views.export, name="export"),
-    url(r'^event/$', views.event, name="event"),
-    url(r'^download/$', views.download, name="download"),
+    re_path(r'^$', views.index, name="index"),
+    re_path(r'^login/$', views.login_view, name="login"),
+    re_path(r'^logout/$', views.logout_view, name="logout"),
+    re_path(r'^speech/$', views.speech, name="speech"),
+    re_path(r'^interview/$', views.interview, name="interview"),
+    re_path(r'^export/$', views.export, name="export"),
+    re_path(r'^event/$', views.event, name="event"),
+    re_path(r'^download/$', views.download, name="download"),
 
-    url(r'^students/$', views.student_panel_view, name="student_panel"),
-    url(r'^students/delete/$', views.student_delete, name="student_delete"),
-    url(r'^students/edit/$', views.student_edit, name="student_edit"),
-    url(r'^students/create/$', views.student_create, name="student_create"),
-    url(r'^students/bulk_create/$', views.students_create, name='students_create'),
+    re_path(r'^students/$', views.student_panel_view, name="student_panel"),
+    re_path(r'^students/delete/$', views.student_delete, name="student_delete"),
+    re_path(r'^students/edit/$', views.student_edit, name="student_edit"),
+    re_path(r'^students/create/$', views.student_create, name="student_create"),
+    re_path(r'^students/bulk_create/$', views.students_create, name='students_create'),
 
-    url(r'^judges/$', views.judge_panel_view, name="judge_panel"),
-    url(r'^judges/delete/$', views.judge_delete, name="judge_delete"),
-    url(r'^judges/edit/$', views.judge_edit, name="judge_edit"),
-    url(r'^judges/create/$', views.judge_create, name="judge_create"),
-    url(r'^judges/bulk_create/$', views.judges_create, name="judges_create"),
+    re_path(r'^judges/$', views.judge_panel_view, name="judge_panel"),
+    re_path(r'^judges/delete/$', views.judge_delete, name="judge_delete"),
+    re_path(r'^judges/edit/$', views.judge_edit, name="judge_edit"),
+    re_path(r'^judges/create/$', views.judge_create, name="judge_create"),
+    re_path(r'^judges/bulk_create/$', views.judges_create, name="judges_create"),
     # url(r'^find_student/$', views.find_student, name="find_student")
 
     # url(r'^times/$', views.times_panel_view, name='times_panel'),
@@ -44,10 +31,10 @@ urlpatterns = [
     # url(r'^times/edit/$', views.time_edit, name='time_edit'),
     # url(r'^times/create/$', views.time_create, name='time_create'),
 
-    url(r'^assignments/$', views.assignments_view, name='assignments_view'),
-    url(r'^assignments/create/$', views.assignment_create, name='assignment_create'),
-    url(r'^assignments/bulk_create/$', views.assignments_create, name='assignments_create'),
-    url(r'^assignments/edit/$', views.assignment_edit, name='assignment_edit'),
-    url(r'^assignments/delete/$', views.assignment_delete, name='assignment_delete')
+    re_path(r'^assignments/$', views.assignments_view, name='assignments_view'),
+    re_path(r'^assignments/create/$', views.assignment_create, name='assignment_create'),
+    re_path(r'^assignments/bulk_create/$', views.assignments_create, name='assignments_create'),
+    re_path(r'^assignments/edit/$', views.assignment_edit, name='assignment_edit'),
+    re_path(r'^assignments/delete/$', views.assignment_delete, name='assignment_delete')
 
 ]
